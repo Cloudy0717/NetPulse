969426bc05a87ddf80f94ccafe8b0442f2681e27
Cloudy0717 <tucksonquah@gmail.com>
2026-07-17 17:10:08 +0800

    fix(v2): use Layout component with Header + Outlet, move BrowserRouter to main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Sidebar from '@/components/layout/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Layout from '@/components/layout/Layout'
 import DashboardPage from '@/pages/DashboardPage'
 import NetworkPage from '@/pages/NetworkPage'
 import SpeedtestPage from '@/pages/SpeedtestPage'
@@ -11,23 +11,18 @@ import SettingsPage from '@/pages/SettingsPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex h-screen bg-slate-950 text-white">
-        <Sidebar />
-        <main className="flex-1 overflow-auto">
-          <Routes>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/network" element={<NetworkPage />} />
-            <Route path="/speedtest" element={<SpeedtestPage />} />
-            <Route path="/traceroute" element={<TraceroutePage />} />
-            <Route path="/history" element={<HistoryPage />} />
-            <Route path="/ping-history" element={<PingHistoryPage />} />
-            <Route path="/port-scanner" element={<PortScannerPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/network" element={<NetworkPage />} />
+        <Route path="/speedtest" element={<SpeedtestPage />} />
+        <Route path="/traceroute" element={<TraceroutePage />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/ping-history" element={<PingHistoryPage />} />
+        <Route path="/port-scanner" element={<PortScannerPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Route>
+    </Routes>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { SettingsProvider } from "./contexts/SettingsContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -7,10 +8,12 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SettingsProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </SettingsProvider>
+    <BrowserRouter>
+      <SettingsProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </SettingsProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
